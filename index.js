cfeb7cffe60a1d8d3089e9cad981b58c3b232b3e
Mychael M <mychaelbmenges@gmail.com>
2021-01-17 17:06:30 -0700

    setup express server with welcome message

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(express.json())
+
+server.get("/", (req, res, next) => {
+  res.json({
+    message: "Hello my dude"
+  })
+})
+
+server.use((err, req, res, next) => {
+  console.log(err)
+
+  res.status(500).json({
+    message: "Something is wrong here"
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is up at http://localhost:${port}`)
+})
