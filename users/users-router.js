dccf1cb82f482dbaccb8cdf637eb0c265e9ac873
Mychael M <mychaelbmenges@gmail.com>
2021-01-17 17:35:14 -0700

    add routes for add new user and hashing password before storing in db

--- users/users-router.js
+++ users/users-router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const Users = require('./users-model')
 
 const router = express.Router()
@@ -7,6 +8,37 @@ router.get("/users", async (req, res, next) => {
   try{
     res.status(200).json(await Users.find())
   } catch(err){
+    next(err)
+  }
+})
+
+router.get("/users/:id", async (req, res, next) => {
+  try{
+    res.status(200).json(await Users.findByID(req.params.id))
+  } catch(err){
+    next(err)
+  }
+})
+
+router.post("/users", async (req, res, next) => {
+  try{
+    const {username, password, department} = req.body
+    const user = await Users.findByUsername(username)
+
+    if(user) {
+      return res.status(409).json({
+        message: "Username is already in the system silly"
+      })
+    }
+
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 15),
+      department
+    })
+
+    res.status(201).json(newUser)
+  } catch(err) {
     next(err)
   }
 })
